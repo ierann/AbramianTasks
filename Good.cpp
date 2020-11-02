a31e504cccd9f18d882d443c9f8dd1b9fa77ed71
ochaplen <o.chaplenko@ukr.net>
2020-11-02 20:55:08 +0200

    Version #3 Proc27(), IsPowerN()

--- Good.cpp
+++ Good.cpp
@@ -1,12 +1,40 @@
 ﻿#include <iostream>
 using namespace std;
 
+bool IsPowerN(int k, int n);
 bool IsPower(int k);
-void proc26();
+void proc27();
 
 int main()
 {
-    proc26();
+    proc27();
+}
+
+void proc27()
+{
+    /*Описать функцию IsPowerN(K, N ) логического типа, возвращающую
+    TRUE, если целый параметр K (> 0) является степенью числа N (> 1), и
+    FALSE в противном случае. Дано число N (> 1) и набор из 10 целых по-ложительных чисел. С помощью функции IsPowerN найти количество
+    степеней числа N в данном наборе.*/
+
+    int k, n;
+    cout << "Enter K: "; cin >> k;
+    cout << "Enter N: "; cin >> n;
+    bool ok = IsPowerN(k, n);
+    cout << "Answer = " << ok << endl;
+}
+
+bool IsPowerN(int k, int n)
+{
+    while (k > 1)
+    {
+        if (k % n != 0)
+        {
+            return false;
+        }
+        k = k / n;
+    }
+    return true;
 }
 
 void proc26()
