6b1492aa596f1f754ca93ab44ec681fc64d1b61b
ochaplen <o.chaplenko@ukr.net>
2020-11-02 20:41:13 +0200

    Version #2 New function: IsPower5(), Proc26()

--- Good.cpp
+++ Good.cpp
@@ -1,6 +1,37 @@
 ﻿#include <iostream>
+using namespace std;
+
+bool IsPower(int k);
+void proc26();
 
 int main()
 {
-    std::cout << "Hello World!!!\n";
+    proc26();
+}
+
+void proc26()
+{
+    /*Описать функцию IsPower5(K ) логического типа, возвращающую
+    TRUE, если целый параметр K (> 0) является степенью числа 5, и FALSE в
+    противном случае. С ее помощью найти количество степеней числа 5 в
+    наборе из 10 целых положительных чисел.*/
+
+    int k;
+    cout << "Enter K: "; cin >> k;
+    bool ok = IsPower(k);
+    cout << "Answer = " << ok << endl;
+}
+
+// 125 = 1 * 5 * 5 * 5 asd
+bool IsPower(int k)
+{
+    while (k > 1)
+    {
+        if (k % 5 != 0)
+        {
+            return false;
+        }
+        k = k / 5;
+    }
+    return true;
 }
